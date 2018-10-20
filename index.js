404b4085146740775a35c131d8f2df3e46f79e07
Ben Rosner <br305893@gmail.com>
2018-10-20 22:03:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {
+'Sugar Cookies': ["Sugar", "Milk", "Flour", "Eggs"],
+'Chocolate Chip Cookies': ["Chocolate Chips", "Milk", "Flour", "Eggs"]
+}
+
+Objects updateObjectWithKeyAndValue (object, key, value){
+return object[key: value]
+}
