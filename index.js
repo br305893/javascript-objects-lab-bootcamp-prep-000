4eba7209a568ca3ec13770aeffc49be169ecf9d6
Ben Rosner <br305893@gmail.com>
2018-10-20 22:14:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 object[key] = value
 return object
 }
+
+function deleteFromObjectByKey(object, key){
+delete Object.key
+return object
+}
